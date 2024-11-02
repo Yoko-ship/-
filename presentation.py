ea983bcdcd0b8d3ce20604bf956a6ffb63e70240
User <yuko.brandon13@gmail.com>
2024-11-02 15:14:47 +0500

    updated

--- presentation.py
+++ presentation.py
@@ -1,23 +1,27 @@
 import google.generativeai as genai
-import pyperclip
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.edge.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import tkinter as tk
+import time
 
 api_key = "AIzaSyCrj3saz9DtSmuesXjHKLR7HIAxRJD3RrY"
+api_captcha = "5d8752a4fa70e6fe7296a3f280e3c44a"
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 user_input = ""
 def get_present():
     global user_input
     user_input = entry.get()
-    if user_input:
+    if len(user_input) >= 3:
         root.destroy()
         error_label.forget()
+    elif len(user_input) < 3:
+        warning_label.pack(pady=5)
+        user_input = None
+        error_label.forget()
+
     else:
         error_label.pack()
         user_input = None
@@ -35,6 +39,7 @@ entry = tk.Entry(root, font=("Helvetica", 14), width=30, bd=2, relief="solid")
 entry.pack(pady=10)  
 error_label = tk.Label(root, text="Пожалуйста, напишите тему", fg="red", bg="#f0f0f0", font=("Helvetica", 10))
 error_label.pack(pady=5)
+warning_label = tk.Label(root,text="Ваш текст должен содержать как минимум 3 символов",fg="yellow",bg="black",font=("Helvetica",10))
 
 button = tk.Button(root, text="Подтвердить", fg="white", bg="#28a745", font=("Helvetica", 12), command=get_present)
 button.pack(pady=15)
@@ -43,7 +48,7 @@ button.config(cursor="hand2")
 root.mainloop()
 
 if user_input:
-    driver = webdriver.Chrome()
+    driver = webdriver.ChromiumEdge()
     driver.get("https://slidesgo.com/")
     elem = driver.find_element(By.ID,"ai-animated")
     elem.click()
@@ -59,4 +64,16 @@ if user_input:
     input_number.send_keys(14)
     input_element.send_keys(user_input)
     input_element.send_keys(Keys.RETURN)
+    signing_up = driver.find_element(By.CSS_SELECTOR,".have-account a")
+    signing_up.click()
+    google_continue = driver.find_element(By.CSS_SELECTOR,".continue-with button:nth-of-type(2)")
+    google_continue.click()
+    input_auth = driver.find_element(By.CSS_SELECTOR,".label input")
+    input_auth.send_keys(f"oxbdtin885@gmail.com")
+    password_auth = driver.find_element(By.NAME,"password")
+    password_auth.send_keys("23132asdagaasfk")
+    time.sleep(2)
+    password_auth.send_keys(Keys.RETURN)
+
+    
 
